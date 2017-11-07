cebd151381bd4f747eced9e50c7f79ab42fad008
aghzousaid <32544746+aghzousaid@users.noreply.github.com>
2017-11-07 10:52:56 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+      // Initialisation des variables
+      var q  = 0;
+      var q1 = 0;
+      var q2 = 0;
+      var q3 = 0;
+      var q4 = 0;
+      var q5 = 0;
+      var q6 = 0;
+      var q7 = 0;
+      var q8 = 0;
+      var q9 = 0;
+      
+      
+      // calcul du score
+      function calculer () {
+      score = q + q1 + q2 + q3 + q4 + q5 + q6 + q7 + q8 + q9 ;
+      alert(" Votre score est :" + score)
+      }
+   
